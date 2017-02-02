7a31e0d5e1243bcb954350f3e7d621749518a204
liukefei <18500406408@163.com>
2017-02-02 21:30:46 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello Tenxcloud-20170101! 12:2");
+  response.end("Hello Tenxcloud-20170102! 12:2");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
